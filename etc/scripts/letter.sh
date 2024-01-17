bb6fce0ae84e0f7010b1e5e7c0bff64470bce45e
Thien <nguyenhthien2001@gmail.com>
2024-01-17 13:34:41 -0600

    cleaned up some code

--- etc/scripts/letter.sh
+++ etc/scripts/letter.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
-#include content of sub and pub script
-source ./sub.sh
+#include content of pub.sh
 source ./pub.sh
 
 #Lift pen
@@ -12,7 +11,7 @@ ros2 service call /turtle1/teleport_absolute turtlesim/srv/TeleportAbsolute "{x:
 #Set pen to red and lower pen
 ros2 service call /turtle1/set_pen turtlesim/srv/SetPen  "{r: 255, g: 0, b: 0, width: 5, 'off': 0}"
 #Draw letter "T"
-pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
+
 pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.0, y: 4.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
 pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: -1.5, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
 pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 3, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
